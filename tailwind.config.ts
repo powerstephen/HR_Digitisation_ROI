795dfed3449a0ea1ebf1efcd036a8ef6440e13d8
powerstephen <power.stephen@gmail.com>
2025-10-26 05:59:52 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      fontFamily: { sans: ["ui-sans-serif", "system-ui", "sans-serif"] },
       colors: {
         factorial: {
           red: "#E51943",
